af8606397eedd2a9e3a7868b78e41954651be579
plgsouza <89405248+plgsouza@users.noreply.github.com>
2021-08-25 17:42:26 -0300

    Update variables.tf
    Acrescentando variaveis banco sql

--- Quality Manager/Terraform/variables.tf
+++ Quality Manager/Terraform/variables.tf
@@ -1,29 +1,47 @@
-//Variaveis RDS
-variable "db_produto" {
+//Variaveis RDS Postgress
+variable "db_produto_postgres" {
   type    = string
   default = "postgres"
 }
 
 
-variable "db_name" {
+variable "db_name_postgres" {
   type    = string
   default = "postgress_quality_manager"
 }
 
 
-variable "db_user" {
+variable "db_user_postgres" {
   type    = string
   default = "postgres"
 }
 
-variable "db_password" {
+variable "db_password_postgres" {
   type    = string
   default = "postgres1234567"
 }
 
-//variaveis ec2
+//variaveis EC2
 
 variable "user_name_ec2" {
   type    = string
   default = "ec2-user"
+}
+
+
+//Variaveis RDS SQLServer
+variable "db_produto_sql" {
+  type    = string
+  default = "sqlserver-ee"
+}
+
+
+variable "db_user_sql" {
+  type    = string
+  default = "admin"
+}
+
+variable "db_password_sql" {
+  type    = string
+  default = "sql1234567"
 }
