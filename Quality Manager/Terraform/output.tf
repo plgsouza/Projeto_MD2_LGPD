339b11ab02b3c80b74ce0597788cdfc1159b1588
plgsouza <89405248+plgsouza@users.noreply.github.com>
2021-08-25 17:41:43 -0300

    Update output.tf
    incluindo dados output

--- Quality Manager/Terraform/output.tf
+++ Quality Manager/Terraform/output.tf
@@ -1,4 +1,4 @@
- output "dns_name_instance_ec2" {
+output "dns_name_instance_ec2" {
   value = aws_instance.quality_manager[0].public_dns
 }
 
@@ -11,14 +11,27 @@ output "id_instance_ec2" {
   value = aws_instance.quality_manager[0].id
 } 
 
-output "ip_instance_rds" {
-  value = aws_db_instance.rds_quality_manager.address
+ output "ip_instance_rds" {
+  value = aws_db_instance.rds_quality_manager_postgress.address
 } 
 
 output "endpoint_instance_rds" {
-  value = aws_db_instance.rds_quality_manager.endpoint
+  value = aws_db_instance.rds_quality_manager_postgress.endpoint
 } 
 
 output "id_instance_rds" {
-  value = aws_db_instance.rds_quality_manager.id
+  value = aws_db_instance.rds_quality_manager_postgress.id
+}  
+ 
+//sql
+output "ip_instance_rds_sql" {
+  value = aws_db_instance.rds_quality_manager_sql.address
+}
+
+output "endpoint_instance_rds_sql" {
+  value = aws_db_instance.rds_quality_manager_sql.endpoint
+}
+
+output "id_instance_rds_sql" {
+  value = aws_db_instance.rds_quality_manager_sql.id
 }  
